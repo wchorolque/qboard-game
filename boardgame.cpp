97d2f2ae0cfafeaefa7d3e8b46365392379b0d36
wchorolque <wchorolque@gmail.com>
2012-12-25 18:27:59 -0400

    Refactored constructor and aggregated some properties

--- boardgame.cpp
+++ boardgame.cpp
@@ -2,16 +2,30 @@
 
 BoardGame::BoardGame()
 {
-    this->setBoardSize(BoardGame::DEFAULT_BOARD_SIZE);
-    this->setGoal(BoardGame::DEFAULT_GOAL);
-    this->setPit(BoardGame::DEFAULT_PIT);
+    this->init(
+                BoardGame::DEFAULT_BOARD_SIZE,
+                BoardGame::DEFAULT_GOAL,
+                BoardGame::DEFAULT_PIT
+                );
 }
 
 BoardGame::BoardGame(qint8 board_size, qint8 goal, qint8 pit)
 {
+    this->init(board_size, goal, pit);
+}
+
+void BoardGame::init(qint8 board_size, qint8 goal, qint8 pit)
+{
     this->setBoardSize(board_size);
     this->setGoal(goal);
     this->setPit(pit);
+
+    this->setNamePlayer01("Player 01");
+    this->setNamePlayer02("Player 02");
+
+    this->setPositionPlayer01(0);
+    this->setPositionPlayer02(0);
+    srand(time(0));
 }
 
 void BoardGame::setBoardSize(qint8 board_size)
@@ -27,7 +41,7 @@ void BoardGame::setBoardSize(qint8 board_size)
     }
 }
 
-qint8 BoardGame::getBoardSize()
+qint8 BoardGame::getBoardSize() const
 {
     return this->board_size;
 }
@@ -40,7 +54,7 @@ void BoardGame::setGoal(qint8 goal)
         this->goal = BoardGame::DEFAULT_GOAL;
     }
 }
- qint8 BoardGame::getGoal()
+ qint8 BoardGame::getGoal() const
  {
      return this->goal;
  }
@@ -54,7 +68,55 @@ void BoardGame::setGoal(qint8 goal)
      }
  }
 
- qint8 BoardGame::getPit()
+ qint8 BoardGame::getPit() const
  {
      return this->pit;
  }
+
+void BoardGame::setPositionPlayer01(qint8 position)
+{
+    if (position >= 0 && position <= this->board_size) {
+        this->position_player01 = position;
+    }
+}
+
+qint8 BoardGame::getPositionPlayer01 () const
+{
+    return this->position_player01;
+}
+
+void BoardGame::setPositionPlayer02(qint8 position)
+{
+    if (position >= 0 && position <= this->board_size) {
+        this->position_player02 = position;
+    }
+}
+
+qint8 BoardGame::getPositionPlayer02() const
+{
+    return this->position_player02;
+}
+
+void BoardGame::setNamePlayer01(QString name)
+{
+    if (name.trimmed().length() > 0) {
+        this->name_player01 = name;
+    }
+}
+
+QString BoardGame::getNamePlayer01() const
+{
+    return this->name_player01;
+}
+
+void BoardGame::setNamePlayer02(QString name)
+{
+    if (name.trimmed().length() > 0) {
+        this->name_player02 = name;
+    }
+}
+
+QString BoardGame::getNamePlayer02() const
+{
+    return this->name_player02;
+}
--- boardgame.h
+++ boardgame.h
@@ -14,20 +14,42 @@ private:
     qint8 goal;
     // Default value 4
     qint8 pit;
+
+    qint8 position_player01;
+    qint8 position_player02;
+
+    QString name_player01;
+    QString name_player02;
+
+    void init(qint8 board_size, qint8 goal, qint8 pit);
 public:
-    BoardGame();
-    BoardGame(qint8 board_size, qint8 goal, qint8 pit);
+    BoardGame ();
+    BoardGame (qint8 board_size, qint8 goal, qint8 pit);
 
-    void setGoal(qint8 goal);
-    qint8 getGoal();
+    void setGoal (qint8 goal);
+    qint8 getGoal () const;
 
-    void setPit(qint8 pit);
-    qint8 getPit();
+    void setPit (qint8 pit);
+    qint8 getPit () const;
 
-    void setBoardSize(qint8 board_size);
-    qint8 getBoardSize();
+    void setBoardSize (qint8 board_size);
+    qint8 getBoardSize () const;
+
+    void setPositionPlayer01 (qint8 position);
+    qint8 getPositionPlayer01 () const;
+
+    void setPositionPlayer02 (qint8 position);
+    qint8 getPositionPlayer02 () const;
+
+    void setNamePlayer01(QString name);
+    QString getNamePlayer01() const;
+
+    void setNamePlayer02(QString name);
+    QString getNamePlayer02() const;
 
     qint8 rollDice();
+    qint8 movePlayer01();
+    qint8 movePlayer02();
 };
 
 
